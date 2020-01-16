b62aa797f39a5b8eaa22849fa9b5d8e532f3014a
Valentin Loiseau <valentin-loiseau@laposte.net>
2020-01-16 12:06:26 +0100

    Allow fields array in getIndexStringStartBy

--- src/DoctrineTool/StringFunctions.php
+++ src/DoctrineTool/StringFunctions.php
@@ -6,13 +6,24 @@ use Doctrine\ORM\QueryBuilder;
 
 class StringFunctions
 {
-    public static function getIndexStringStartBy(QueryBuilder $qb, string $field, string $startBy, ?string $alias = 'indexStartBy', bool $hidden = true): string
+    public static function getIndexStringStartBy(QueryBuilder $qb, $fields, string $startBy, ?string $alias = 'indexStartBy', bool $hidden = true): string
     {
         $hiddenString = $hidden ? 'HIDDEN ' : '';
         $key = Utils::randomString();
 
         $qb->setParameter($key, "$startBy%");
 
-        return "(CASE WHEN {$qb->expr()->like($field, ":$key")} THEN 0 ELSE 1 END) AS $hiddenString$alias";
+        if (is_string($fields)) {
+        	$condition = $qb->expr()->like($fields, ":$key");
+		} elseif (is_array($fields)) {
+			$condition = $qb->expr()->orX();
+			foreach ($fields as $field) {
+				$condition->add($qb->expr()->like($field, ":$key"));
+			}
+		} else {
+        	throw new \LogicException(gettype($fields).' type is not allowed here.');
+		}
+
+        return "(CASE WHEN $condition THEN 0 ELSE 1 END) AS $hiddenString$alias";
     }
 }
